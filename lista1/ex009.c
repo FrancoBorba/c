c7974191c4584050b0df9079a22df769a4c6d92e
FrancoBorba <franco.borba14@gmail.com>
2023-04-06 13:55:22 -0300

    new

--- lista1/ex009.c
+++ lista1/ex009.c
@@ -14,16 +14,22 @@ int main(){
 
     delta = pow(b,2) -4*a*c;
 
-    if(delta < 0){
-        printf("Impossivel calcular\n");
-    }
-   esle{ 
+    if(a==0){
+             printf("Impossivel calcular\n");
+            }
+
+    else 
+        if(delta < 0){
+             printf("Impossivel calcular\n");
+                      }
+
+        else{ 
         r1 = (-b + sqrt(delta))/(2*a);
         r2 = (-b - sqrt(delta))/(2*a);
 
              printf("R1 = %.5lf\n",r1);
              printf("R2 = %.5lf\n",r2);
-    }
+            }
 
     return 0;
 
