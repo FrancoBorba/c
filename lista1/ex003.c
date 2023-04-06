044eac31f87ac513cf7d0da274af3e73661d6e1b
FrancoBorba <franco.borba14@gmail.com>
2023-04-05 23:33:37 -0300

    Exercicio3

---
+++ lista1/ex003.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+
+    float a , b, c ,media;
+
+    printf("Digite a primeira nota\n");
+    scanf("%f" , &a);
+    printf("Digite a segunda nota\n");
+    scanf("%f" , &b);
+    printf("Digite a terceira nota\n");
+    scanf("%f", &c);
+    media = (2*a + 3*b + 5*c)/10;
+    printf("MEDIA = %.1f" , media);
+
+    
+
+}
