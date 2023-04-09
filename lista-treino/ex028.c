28853b6f6ed05b308f96200b5d922579c88346b1
FrancoBorba <franco.borba14@gmail.com>
2023-04-09 20:43:54 -0300

    new

--- lista-treino/ex028.c
+++ lista-treino/ex028.c
@@ -4,17 +4,21 @@
 // 1 litro para cada 3 metros quadrados
 int main(){
 
-    int area, litros, total, quantidade ;
+    int area, litros, total ;
 
-    printf("Digite a area a ser pintada");
+    printf("Digite a area a ser pintada\n");
     scanf("%i",&area);
 
     litros = area/3;
-    quantidade = litros/18;
-    total = litros*quantidade;
-
-    // apaguei dps eu tento
-
-  
     
+
+    if(litros/18 == 0){
+        total = 80;
+                    }
+    else{
+        total = 80*(litros/18);
+        }
+   // printf("O total a ser comprado foi = %i\n",total);
+  printf("%i",litros/18);
+    return 0;
 }
